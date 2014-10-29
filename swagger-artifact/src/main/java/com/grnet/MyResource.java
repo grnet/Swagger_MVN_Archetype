d342378ab8300f90673e94f31f846b9efbc3184c
vogias <vogias@admin.grnet.gr>
2014-10-29 13:26:13 +0200

    initial commit

---
+++ swagger-artifact/src/main/java/com/grnet/MyResource.java
@@ -0,0 +1,40 @@
+package com.grnet;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiParam;
+
+/**
+ * Example resource class hosted at the URI path "/myresource"
+ */
+@Path("/service")
+@Api(value = "/service", description = "Service descirption goes here")
+public class MyResource {
+
+	@GET
+	@Path("/verb1")
+	@ApiOperation(value = "verb1 operation", notes = "verb1 operation notes.")
+	@Produces("text/plain")
+	public String getIt(
+			@ApiParam(value = "parameter1", required = true) @QueryParam("query parameterer") String repo) {
+
+
+		return "verb 1";
+	}
+
+	@GET
+	@Path("/verb2")
+	@ApiOperation(value = "Verb2 Operation")
+	@Produces("text/plain")
+	public String list() {
+
+		return "verb2";
+
+	}
+
+}
